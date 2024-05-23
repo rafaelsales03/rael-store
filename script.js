f0c27d602489fae915fdc29430115e9549914c5c
rafaelsales03 <elrafael76234@gmail.com>
2024-05-23 17:25:34 -0300

    add products details

--- script.js
+++ script.js
@@ -116,3 +116,34 @@ const carousel = new Swiper('.carouselbox', {
   
    
   });
+
+
+  //product image > page-single
+
+  const thumbImage = new Swiper('.thumbnail-image', {
+    
+    // loop: true,
+    direction: 'vertical',
+    spaceBetween: 15,
+    slidesPerView: 1,
+    freeMode: true,
+    watchSlidesProgress: true,
+  
+});
+
+const mainImage = new Swiper('.main-image', {
+    
+    loop: true,
+    autoHeight: true,
+  
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      
+      thumbs: {
+          swiper: thumbImage,
+      }
+  });
+
+
